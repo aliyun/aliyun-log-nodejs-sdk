3658977b04ab716ee0bc876aa4cc9afd514f16cd
Jackson Tian <shyvo1987@gmail.com>
2017-08-08 00:24:35 +0800

    complete CRUD for logstore

--- test/client.test.js
+++ test/client.test.js
@@ -4,7 +4,26 @@ const expect = require('expect.js');
 
 const Client = require('../lib/client');
 
+// cn-hangzhou.log.aliyuncs.com
+const client = new Client({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  accessKeySecret: process.env.ACCESS_KEY_SECRET,
+  region: 'cn-shanghai',
+  project: 'my-fc-log'
+});
+const logstoreName = `test-logs-${Date.now()}`;
+
 describe('client', function () {
+  it('constructor', function () {
+    const client = new Client({
+      accessKeyId: "bq2sjzesjmo86kq35behupbq",
+      accessKeySecret: "4fdO2fTDDnZPU/L7CHNdemB2Nsk=",
+      region: 'cn-hangzhou',
+      project: 'my-fc-log'
+    });
+    expect(client.endpoint).to.be('http://my-fc-log.cn-hangzhou.log.aliyuncs.com');
+  });
+
   it('sign GET', function () {
     const client = new Client({
       accessKeyId: "bq2sjzesjmo86kq35behupbq",
@@ -42,5 +61,53 @@ describe('client', function () {
       'x-log-compresstype': 'lz4'
     });
     expect(sign).to.be('LOG bq2sjzesjmo86kq35behupbq:XWLGYHGg2F2hcfxWxMLiNkGki6g=');
+  });
+
+  it('listLogstore should ok', async function () {
+    var result = await client.listLogstore();
+    expect(result.statusCode).to.be(200);
+    var body = result.body;
+    expect(body).to.have.property('count');
+    expect(body).to.have.property('logstores');
+    expect(body.logstores.length).to.above(0);
+    expect(body).to.have.property('total');
+  });
+
+  it('createLogstore should ok', async function () {
+    var result = await client.createLogstore(logstoreName, {
+      ttl: 10,
+      shardCount: 2
+    });
+    expect(result.statusCode).to.be(200);
+    expect(result.body).to.be('');
+    var r = await client.getLogstore(logstoreName);
+    expect(r.body.logstoreName).to.be(logstoreName);
+    expect(r.body.ttl).to.be(10);
+  });
+
+  it('updateLogstore should ok', async function () {
+    var result = await client.updateLogstore(logstoreName, {
+      ttl: 20,
+      shardCount: 2
+    });
+    expect(result.statusCode).to.be(200);
+    expect(result.body).to.be('');
+    var r = await client.getLogstore(logstoreName);
+    expect(r.body.logstoreName).to.be(logstoreName);
+    expect(r.body.ttl).to.be(20);
+  });
+
+  it('deleteLogstore should ok', async function () {
+    var result = await client.deleteLogstore(logstoreName);
+    expect(result.statusCode).to.be(200);
+    expect(result.body).to.be('');
+    try {
+      var r = await client.getLogstore(logstoreName);
+    } catch (ex) {
+      expect(ex.code).to.be('LogStoreNotExist');
+      return;
+    }
+
+    expect(new Error('should not ok'));
   });
 });
