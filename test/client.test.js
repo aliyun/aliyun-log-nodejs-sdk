edbade43bc77c9d6e5b61e150ee788c235c9fd27
Jackson Tian <shyvo1987@gmail.com>
2017-08-07 22:11:41 +0800

    Add more test case for sign

--- test/client.test.js
+++ test/client.test.js
@@ -5,7 +5,7 @@ const expect = require('expect.js');
 const Client = require('../lib/client');
 
 describe('client', function () {
-  it('sign', function () {
+  it('sign GET', function () {
     const client = new Client({
       accessKeyId: "bq2sjzesjmo86kq35behupbq",
       accessKeySecret: "4fdO2fTDDnZPU/L7CHNdemB2Nsk="
@@ -22,5 +22,25 @@ describe('client', function () {
       'x-log-signaturemethod': 'hmac-sha1'
     });
     expect(sign).to.be('LOG bq2sjzesjmo86kq35behupbq:jEYOTCJs2e88o+y5F4/S5IsnBJQ=');
+  });
+
+  it('sign POST', function () {
+    const client = new Client({
+      accessKeyId: "bq2sjzesjmo86kq35behupbq",
+      accessKeySecret: "4fdO2fTDDnZPU/L7CHNdemB2Nsk="
+    });
+
+    // sign(verb, path, queries, headers) {
+    const sign = client.sign('POST', '/logstores/test-logstore', {}, {
+      date: 'Mon, 09 Nov 2015 06:03:03 GMT',
+      'x-log-apiversion': '0.6.0',
+      'x-log-signaturemethod': 'hmac-sha1',
+      'content-md5': '1DD45FA4A70A9300CC9FE7305AF2C494',
+      'content-length': '52',
+      'content-type': 'application/x-protobuf',
+      'x-log-bodyrawsize': '50',
+      'x-log-compresstype': 'lz4'
+    });
+    expect(sign).to.be('LOG bq2sjzesjmo86kq35behupbq:XWLGYHGg2F2hcfxWxMLiNkGki6g=');
   });
 });
