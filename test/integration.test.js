44ef42207a333ad3cbf3525a8a4d4c91ae4bad31
洗影 <qiuyi.zqy@alibaba-inc.com>
2017-08-09 17:19:13 +0800

    lib: log index CRUD and getLogs

--- test/integration.test.js
+++ test/integration.test.js
@@ -4,13 +4,24 @@ const assert = require('assert');
 const Client = require('../lib/client');
 
 const testProject = process.env.TEST_PROJECT;
+const testStore = process.env.TEST_STORE;
+const testStore2 = process.env.TEST_STORE2;
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const accessKeySecret = process.env.ACCESS_KEY_SECRET;
 const PROJECT_DELAY = 1500;
 
+// Due to a bug in SLS we need to use a existing project to test log store CRUD
 assert.strictEqual(typeof testProject, 'string',
   'set TEST_PROJECT envrinoment variable to an existing log project ' +
   'before running the integration test');
+// Due to the delay we must use an existing store to test log index CRUD
+assert.strictEqual(typeof testStore, 'string',
+  'set TEST_STORE envrinoment variable to an existing log store ' +
+  'before running the integration test');
+// Due to the delay we must use an existing store with index to test log CRUD
+assert.strictEqual(typeof testStore2, 'string',
+  'set TEST_STORE2 envrinoment variable to an existing log store ' +
+  'with an index before running the integration test');
 assert.strictEqual(typeof accessKeyId, 'string',
   'set ACCESS_KEY_ID envrinoment variable before running the integration test');
 assert.strictEqual(typeof accessKeySecret, 'string', 
@@ -24,14 +35,36 @@ const client = new Client({
   region: 'cn-shanghai'
 });
 
+const index = {
+  "ttl": 7,
+  "keys": {
+    "functionName": {
+      "caseSensitive": false,
+      "token": [ "\n", "\t", ";", ",", "=", ":" ],
+      "type": "text"
+    }
+  },
+};
+
+const index2 = {
+  "ttl": 7,
+  "keys": {
+    "serviceName": {
+      "caseSensitive": false,
+      "token": [ "\n", "\t", ";", ",", "=", ":" ],
+      "type": "text"
+    }
+  },
+};
+
 function sleep(timeout) {
   return new Promise((resolve, reject) => {
     setTimeout(() => resolve(), timeout);
   });
 }
 
-describe('Integration test', function () {
-  describe('log project CRUD', function () {
+describe('Integration test', async function () {
+  describe('log project CRUD', async function () {
     const projectName = `test-project-${Date.now()}`;
 
     it('createProject should ok', async function () {
@@ -61,8 +94,7 @@ describe('Integration test', function () {
     });
   });
 
-  describe('log store CRUD', function () {
-    // Due to a bug in SLS we need to use a existing project to test this
+  describe('log store CRUD', async function () {
     const logstoreName = `test-logs-${Date.now()}`;
 
     it('createLogstore should ok', async function () {
@@ -111,6 +143,53 @@ describe('Integration test', function () {
       }
 
       assert.fail('The log store should have been deleted');
+    });
+  });
+
+  describe('log index', async function() {
+    it('createIndex should ok', async function() {
+      var result = await client.createIndex(testProject, testStore, index);
+      assert.strictEqual(result.statusCode, 200);
+      var result = await client.getIndexConfig(testProject, testStore);
+      assert.strictEqual(result.statusCode, 200);
+      // The effective TTL is always the same as the one in the
+      // log project config, setting it here does not affect the config
+      assert.strictEqual(typeof result.body.ttl, "number");
+      assert.deepStrictEqual(result.body.keys, index.keys);
+    });
+
+    it('updateIndex should ok', async function() {
+      var result = await client.updateIndex(testProject, testStore, index2);
+      assert.strictEqual(result.statusCode, 200);
+      var result = await client.getIndexConfig(testProject, testStore);
+      assert.strictEqual(result.statusCode, 200);
+      assert.deepStrictEqual(result.body.keys, index2.keys);
+    });
+
+    it('deleteIndex should ok', async function() {
+      var result = await client.deleteIndex(testProject, testStore);
+      assert.strictEqual(result.statusCode, 200);
+      assert.strictEqual(result.body, '');
+      try {
+        var r = await client.getIndexConfig(testProject, testStore);
+      } catch (ex) {
+        assert.strictEqual(ex.code, 'IndexConfigNotExist');
+        return;
+      }
+
+      assert.fail('The log index should have been deleted');
+    });
+  });
+
+  describe('getLogs', async function () {
+    const from = new Date();
+    from.setDate(from.getDate() - 1);
+    const to = new Date();
+
+    it('getLogs should ok', async function () {
+      var result = await client.getLogs(testProject, testStore2, from, to);
+      assert.strictEqual(result.statusCode, 200);
+      assert.strictEqual(Array.isArray(result.body), true);
     });
   });
 });
