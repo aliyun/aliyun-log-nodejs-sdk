e0648e2fc7e213aa3c7d66af29df7b79d8b39b02
sang4lv <idu.angela@gmail.com>
2019-11-20 17:19:46 +0800

    chore: support STS token

--- test/unit.test.js
+++ test/unit.test.js
@@ -54,4 +54,28 @@ describe('Unit test', function () {
     assert.strictEqual(sign,
       'LOG bq2sjzesjmo86kq35behupbq:XWLGYHGg2F2hcfxWxMLiNkGki6g=');
   });
+
+  it('client#_sign should sign POST requests correctly with STS token', function () {
+    const credentials = {
+      accessKeyId: 'STS.NSNYgJ2KUoYaEuDrNazRLg2a6',
+      accessKeySecret: '56Xqw2THF5vTHTNkGWR6uGRKXToKWMi2eLFjppPNV8RR',
+      securityToken: 'CAISiwJ1q6Ft5B2yfSjIr5D7Et3+35R02JuKR1P1lk40dt1giPfK1Dz2IHhLdXNrAuEXs/w0mmBQ7v8TlqZdVplOWU3Da+B364xK7Q75jHw5B0zwv9I+k5SANTW5KXyShb3/AYjQSNfaZY3eCTTtnTNyxr3XbCirW0ffX7SClZ9gaKZ8PGD6F00kYu1bPQx/ssQXGGLMPPK2SH7Qj3HXEVBjt3gX6wo9y9zmnZHNukGH3QOqkbVM9t6rGPX+MZkwZqUYesyuwel7epDG1CNt8BVQ/M909vccpmad5YrMUgQJuEvWa7KNo8caKgJmI7M3AbBFp/WlyKMn5raOydSrkE8cePtSVynP+g0hR0dZ+YgagAEFdb+5rO1e+OZ3kcmPKF5Zh2Sni+vF1qzKA/SElND5koQQV6uvVCweKnfzCPMKjY0OXWmfgtcwOTyJ4ABGsTGnILzBNRD/+Gdqe7wclZrj0aDUkTdFf8k7SudZuO9KOPBe8mS3pJoMs1p67mWA/J4Wn0dottbprb5EQOBRxUC6bw=='
+    };
+    const client = new Client(credentials);
+
+    // sign(verb, path, queries, headers) {
+    const sign = client._sign('POST', '/logstores/test-logstore', {}, {
+      date: 'Mon, 09 Nov 2015 06:03:03 GMT',
+      'x-log-apiversion': '0.6.0',
+      'x-log-signaturemethod': 'hmac-sha1',
+      'content-md5': '1DD45FA4A70A9300CC9FE7305AF2C494',
+      'content-length': '52',
+      'content-type': 'application/x-protobuf',
+      'x-log-bodyrawsize': '50',
+      'x-log-compresstype': 'lz4',
+      'x-acs-security-token': credentials.securityToken,
+    });
+    assert.strictEqual(sign,
+      'LOG STS.NSNYgJ2KUoYaEuDrNazRLg2a6:G3R03b6PwVI+zUaLtqezsBDL/j8=');
+  });
 });
